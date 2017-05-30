17688d3f53bbe27ec69d4f83c9641e39b926dc26
Nicholas McDonald <nmmcdona@ucsd.edu>
2017-05-30 16:42:20 -0700

    Test responding with first name

--- app.py
+++ app.py
@@ -53,17 +53,19 @@ def webhook():
     return "ok", 200
 
 def determineResponse(recipient_id, message_text):
-    replyDict = {
-        "Make an appointment at Peter's Hotness Boutique at 5pm on Thursday.": "Your appointment has been set for Peter's Hotness Boutique! 5pm on Thursday, June 1st!",
-        "Thanks!":"You're welcome!",
-        "Remind me of my appointment on Thursday.":"Okay, I will remind you at 3pm on Thursday, June 1st about your appointment at Peter's Hotness Boutique."
-    }
-    # params = {
-    #     "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    # replyDict = {
+    #     "Make an appointment at Peter's Hotness Boutique at 5pm on Thursday.": "Your appointment has been set for Peter's Hotness Boutique! 5pm on Thursday, June 1st!",
+    #     "Thanks!":"You're welcome!",
+    #     "Remind me of my appointment on Thursday.":"Okay, I will remind you at 3pm on Thursday, June 1st about your appointment at Peter's Hotness Boutique."
     # }
-    # r = requests.post("https://graph.facebook.com/v2.6/{recipient}".format(recipient=recipient_id), params=params)
-    #
-    return replyDict.get(message_text, "Enque is processing your reuqest.")
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    r = requests.post("https://graph.facebook.com/v2.6/{recipient}".format(recipient=recipient_id), params=params)
+    normalized_response = r.json()
+    return normalized_response["first_name"]
+
+    #return replyDict.get(message_text, "Enque is processing your reuqest.")
 
 def send_message(recipient_id, message_text):
 
