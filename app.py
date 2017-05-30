c5b172292ee55f07595501d67a8934282aa639cc
Nicholas McDonald <nmmcdona@ucsd.edu>
2017-05-30 16:59:52 -0700

    Test responding with first name

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"].get("text", "no text provided")  # the message's text
-                    message_to_send = determineResponse(recipient_id, message_text)
+                    message_to_send = determineResponse(recipient_id, sender_id)
                     send_message(sender_id, message_to_send)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
